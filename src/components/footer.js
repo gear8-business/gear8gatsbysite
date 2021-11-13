219c011dfaf871cf7e2d054a6d6f0bf1614243e5
gear8-business <93443043+gear8-business@users.noreply.github.com>
2021-11-13 18:53:04 +0530

    Footer changed

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,7 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      Built with <a href="https://gear8.in/">Gear8 </a> &cpy; 2021-2022
     </div>
   </Container>
 )
